22e8979c82eb0ac4e872505773252ca2a1a0678d
John Temple <johntemple@Alenas-iMac.local>
2021-02-04 23:42:40 -0700

    Relocated and Created Registration page and styled

--- frontend/client/src/config/routes.js
+++ frontend/client/src/config/routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavBar from "../components/NavBar";
+import NavBar from "../components/Navbar/NavBar";
 import Footer from "../components/Footer";
 import LandingPage from "../pages/Landing/LandingPage";
 import Workouts from "../components/workouts/Workouts";
@@ -10,6 +10,8 @@ import Articles from "../components/articles/Articles";
 import EditArticle from "../components/articles/EditArticle";
 import NewArticle from "../components/articles/NewArticle";
 import NewUser from "../components/user/NewUser";
+import Login from '../pages/Login/Login';
+import Register from "../pages/Register/Register";
 
 
 
@@ -24,6 +26,8 @@ export default (
     <Route path="/articles/update/:id" exact component={(EditArticle)} />
     <Route path="/articles/new" exact component={(NewArticle)} />
     <Route path="/user/new" exact component={(NewUser)} />
+    <Route path="/login" exact component={(Login)}/>  
+    <Route path="/register" exact component={(Register)}/>  
     {/* <Route path="/user/:id" exact component={(ShowUser)} /> */}
     {/* <Footer/> */}
   </Router>
---
+++ frontend/client/src/pages/Register/Register.js
@@ -0,0 +1,134 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "./Register.css";
+import Logo from "../../images/logo.png";
+
+class NewUser extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangeGoal = this.onChangeGoal.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: "",
+      email: "",
+      goal: "",
+      password: "",
+    };
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value,
+    });
+  }
+  onChangeGoal(e) {
+    this.setState({
+      goal: e.target.value,
+    });
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username,
+      email: this.state.email,
+      goal: this.state.goal,
+      password: this.state.password,
+    };
+
+    console.log(user);
+
+    axios
+      .post("http://localhost:4000/api/users/add", user)
+      .then((res) => console.log(res.data))
+      .catch((error) => console.log(error));
+
+    this.setState({
+      username: "",
+      email: "",
+      goal: "",
+      password: "",
+    });
+    // add an onSuccess redirect to home
+    window.location = "/";
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <input
+              placeholder="First Name"
+              type="text"
+              required
+              className="form-control"
+              value={this.state.firstName}
+              onChange={this.onChangeFirstName}
+            />
+            <input
+              placeholder="Last Name"
+              type="text"
+              required
+              className="form-control"
+              value={this.state.lastName}
+              onChange={this.onChangeLastName}
+            />
+            <input
+              placeholder="Create a username"
+              type="text"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            />
+            <input
+              placeholder="Enter Email..."
+              type="text"
+              required
+              className="form-control"
+              value={this.state.email}
+              onChange={this.onChangeEmail}
+            />
+
+            <input
+              placeholder="Enter a password"
+              type="password"
+              className="form-control"
+              value={this.state.password}
+              onChange={this.onChangePassword}
+            />
+            <input
+              type="submit"
+              value="Create User"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+        <p className="register__login--link">
+          Already have an account?&nbsp;<a href="/login">Click Here </a>&nbsp;to login!
+        </p>
+      </div>
+    );
+  }
+}
+export default NewUser;
